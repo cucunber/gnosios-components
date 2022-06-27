3072cee790442da0099b05a5c5fc535bc4216309
kalinichenko <ka.ki.ko.1995@gmail.com>
2022-06-27 20:58:46 +0300

    Replaced current path and colors

--- src/dataDisplay/Icon/images/copy.tsx
+++ src/dataDisplay/Icon/images/copy.tsx
@@ -4,9 +4,9 @@ export default {
   sm: (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      viewBox="0 0 16 16">
+      width="18"
+      height="18"
+      viewBox="0 0 18 18">
       <g fill="none" fillRule="evenodd">
         <path
           className="icon-color"
