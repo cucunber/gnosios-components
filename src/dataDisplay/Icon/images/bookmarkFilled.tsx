ed7374bd43f7a6ae47bde6ba8383f5fa0265313e
kalinichenko <ka.ki.ko.1995@gmail.com>
2022-07-05 21:32:13 +0300

    Refactor bookmark icons

--- src/dataDisplay/Icon/images/bookmarkFilled.tsx
+++ src/dataDisplay/Icon/images/bookmarkFilled.tsx
@@ -2,39 +2,47 @@ import React from 'react';
 
 export default {
   sm: (
+    // <svg
+    //   height="16"
+    //   width="12"
+    //   viewBox="0 0 12 12"
+    //   fill="none"
+    //   xmlns="http://www.w3.org/2000/svg">
+    //   <g fill="none" fillRule="evenodd">
+    //     <path
+    //       className="icon-color icon-stroke"
+    //       d="M9 11L5 8.22222L1 11V2.11111C1 1.81643 1.12041 1.53381 1.33474 1.32544C1.54906 1.11706 1.83975 1 2.14286 1H7.85714C8.16025 1 8.45094 1.11706 8.66527 1.32544C8.87959 1.53381 9 1.81643 9 2.11111V11Z"
+    //       strokeWidth="2"
+    //       strokeLinecap="round"
+    //       strokeLinejoin="round"
+    //     />
+    //   </g>
+    // </svg>
     <svg
+      width="16"
       height="16"
-      width="12"
-      viewBox="0 0 12 12"
+      viewBox="0 0 16 16"
       fill="none"
       xmlns="http://www.w3.org/2000/svg">
-      <g fill="none" fillRule="evenodd">
-        <path
-          className="icon-color icon-stroke"
-          d="M9 11L5 8.22222L1 11V2.11111C1 1.81643 1.12041 1.53381 1.33474 1.32544C1.54906 1.11706 1.83975 1 2.14286 1H7.85714C8.16025 1 8.45094 1.11706 8.66527 1.32544C8.87959 1.53381 9 1.81643 9 2.11111V11Z"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </g>
+      <path
+        className="icon-color icon-stroke"
+        d="M17 3H7C6.46957 3 5.96086 3.21071 5.58579 3.58579C5.21071 3.96086 5 4.46957 5 5V21L12 18L19 21V5C19 3.89 18.1 3 17 3Z"
+        fill="currentColor"
+      />
     </svg>
   ),
   md: (
     <svg
+      width="24"
       height="24"
-      width="20"
-      viewBox="0 0 12 12"
+      viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg">
-      <g fill="none" fillRule="evenodd">
-        <path
-          className="icon-color icon-stroke"
-          d="M9 11L5 8.22222L1 11V2.11111C1 1.81643 1.12041 1.53381 1.33474 1.32544C1.54906 1.11706 1.83975 1 2.14286 1H7.85714C8.16025 1 8.45094 1.11706 8.66527 1.32544C8.87959 1.53381 9 1.81643 9 2.11111V11Z"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </g>
+      <path
+        className="icon-color icon-stroke"
+        d="M17 3H7C6.46957 3 5.96086 3.21071 5.58579 3.58579C5.21071 3.96086 5 4.46957 5 5V21L12 18L19 21V5C19 3.89 18.1 3 17 3Z"
+        fill="currentColor"
+      />
     </svg>
   ),
 };
